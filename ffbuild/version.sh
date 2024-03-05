9a48d1ebb7918b0edca10a7b91576db88348ee55
RamonRdL <ramonszanto@gmail.com>
2024-03-05 13:06:51 +0100

    asd

--- ffbuild/version.sh
+++ ffbuild/version.sh
@@ -45,7 +45,7 @@ if [ -z "$2" ]; then
     exit
 fi
 
-NEW_REVISION="#define FFMPEG_VERSION \"2024.02.02 Build by https://www.procontrol.hu/ V: 2.06.11\""
+NEW_REVISION="#define FFMPEG_VERSION \"2024.02.02 Build by https://www.procontrol.hu/ V: 2.06.12\""
 OLD_REVISION=$(cat "$2" 2> /dev/null | head -4 | tail -1)
 
 # String used for preprocessor guard
